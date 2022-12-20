2b923aca78b83d34a9161955d5033b4201d43621
Izzatulsabrinayoo <113493852+Izzatulsabrinayoo@users.noreply.github.com>
2022-12-20 11:05:20 +0700

    Initial commit

---
+++ src/main/java/com/izza/gaji/service/GajiServiceApplication.java
@@ -0,0 +1,18 @@
+package com.izza.gaji.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class GajiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GajiServiceApplication.class, args);
+	}
+        @Bean
+        public RestTemplate restTemplate() {
+            return new RestTemplate();
+        }
+}
